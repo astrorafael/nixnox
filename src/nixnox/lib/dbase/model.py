63023ae440cedcef2d8fdc6e99e1c2597678fea1
astrorafael <astrorafael@gmail.com>
2025-04-23 00:19:54 +0200

    added comments to model

--- src/nixnox/lib/dbase/model.py
+++ src/nixnox/lib/dbase/model.py
@@ -136,26 +136,39 @@ TimestampType: Enum = Enum(
 class Date(Model):
     __tablename__ = "date_t"
 
+    # Date as YYYYMMDD integer
     date_id: Mapped[int] = mapped_column(primary_key=True)
-    sql_date: Mapped[str] = mapped_column(String(10))  # YYYY-MM-DD
-    date: Mapped[str] = mapped_column(String(10))  # DD/MM/YYYY
+    # Date as YYYY-MM-DD string
+    sql_date: Mapped[str] = mapped_column(String(10))
+    # Date as DD/MM/YYYY
+    date: Mapped[str] = mapped_column(String(10))
+    # Day of monty 1..31
     day: Mapped[int]
+    # day of year 1..365
     day_year: Mapped[int]
+    # Julian date at midnight
     julian_day: Mapped[float]
+    # Sunday, Monday, Tuesday, ...
     weekday: Mapped[str] = mapped_column(String(9))
+    # Sun, Mon, Tue, ...
     weekday_abbr: Mapped[str] = mapped_column(String(3))
+    # 0=Sunday, 1=Monday
     weekday_num: Mapped[int]
     month_num: Mapped[int]
-    month: Mapped[str] = mapped_column(String(8))  # January, February, March, ...
-    month_abbr: Mapped[str] = mapped_column(String(3))  # Jan, Feb, Mar, ...
+    # January, February, March, ...
+    month: Mapped[str] = mapped_column(String(8))
+    # Jan, Feb, Mar, ...
+    month_abbr: Mapped[str] = mapped_column(String(3))
     year: Mapped[int]
 
 
 class Time(Model):
     __tablename__ = "time_t"
 
+    # HHMMSS as integer
     time_id: Mapped[int] = mapped_column(primary_key=True)
-    time: Mapped[str] = mapped_column(String(8))  # HH:MM:SS
+    # HH:MM:SS string
+    time: Mapped[str] = mapped_column(String(8))
     hour: Mapped[int]
     minute: Mapped[int]
     second: Mapped[int]
@@ -166,13 +179,21 @@ class Observer(Model):
     __tablename__ = "observer_t"
 
     observer_id: Mapped[int] = mapped_column(primary_key=True)
+    # Either Indiviudal or Organization
     type: Mapped[ObserverEnum] = mapped_column(ObserverEnum, nullable=False)
-    name: Mapped[str] = mapped_column(String(255))  # Full name or organization name
-    nickname: Mapped[Optional[str]] = mapped_column(String(12), nullable=True)  # if person
-    affiliation: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)  # if person
-    acronym: Mapped[Optional[str]] = mapped_column(String(16), nullable=True)  # both types
-    website_url: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)  # both types
-    email: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)  # both types
+    # Full name or organization name
+    name: Mapped[str] = mapped_column(String(255))
+    # Observer nickname for indivoiduals
+    nickname: Mapped[Optional[str]] = mapped_column(String(12), nullable=True)
+    # Observer (individual) affiliation (an organization name)
+    affiliation: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    # Organization acronym
+    acronym: Mapped[Optional[str]] = mapped_column(String(16), nullable=True)
+    # Individual/Organization website URL
+    website_url: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    # Individual/Organization contact email
+    email: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    # Version control attributes for Individuals that change affiliations
     valid_since: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     valid_until: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     valid_state: Mapped[ValidStateType] = mapped_column(ValidStateType, nullable=False)
@@ -184,15 +205,20 @@ class Location(Model):
     __tablename__ = "location_t"
 
     location_id: Mapped[int] = mapped_column(primary_key=True)
-    longitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)  # in decimal degrees
-    latitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)  # in decimal degrees
-    msl: Mapped[Optional[float]]  # metersa above sea level in meters
-    place: Mapped[str] = mapped_column(String(255), nullable=False)  # the actual place name
-    town: Mapped[str] = mapped_column(String(255), nullable=False)  # village, town, city, etc.
-    sub_region: Mapped[str] = mapped_column(String(255), nullable=False)  # province, etc.
-    region: Mapped[str] = mapped_column(
-        String(255), nullable=False
-    )  # fedral state, comunidad autonomica, etc..
+    # Geographical longitude in decimal degrees
+    longitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    # Geographical in decimal degrees
+    latitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    # Meters above sea level
+    msl: Mapped[Optional[float]]
+    # Descriptive name of this unitque location
+    place: Mapped[str] = mapped_column(String(255), nullable=False)
+    # village, town, city, etc name
+    town: Mapped[str] = mapped_column(String(255), nullable=False)  # .
+    # province, county, etc..
+    sub_region: Mapped[str] = mapped_column(String(255), nullable=False)
+    # federal state, comunidad autonomica, etc..
+    region: Mapped[str] = mapped_column(String(255), nullable=False)
     country: Mapped[str] = mapped_column(String(64), nullable=False)
     timezone: Mapped[str] = mapped_column(String(64), nullable=False)
 
@@ -203,8 +229,11 @@ class Flags(Model):
     __tablename__ = "flags_t"
 
     flags_id: Mapped[int] = mapped_column(primary_key=True)
+    # Temperature measurement type
     temperature_meas: Mapped[TemperatureType] = mapped_column(TemperatureType, nullable=False)
+    # Huminity measurement type
     humidity_meas: Mapped[HumidityType] = mapped_column(HumidityType, nullable=False)
+    # Tiemstamp measurement type
     timestamp_meas: Mapped[TimestampType] = mapped_column(TimestampType, nullable=False)
 
 
@@ -212,13 +241,20 @@ class Photometer(Model):
     __tablename__ = "photometer_t"
 
     phot_id: Mapped[int] = mapped_column(primary_key=True)
+    # Either TAS or SQM
     model: Mapped[PhotModelType] = mapped_column(PhotModelType)
+    # Photometer name
     name: Mapped[str] = mapped_column(String(10))
-    identifier: Mapped[str] = mapped_column(String(17))  # SQM serial id or TAS identifier
+    # SQM serial id or TAS identifier (i.e. MAC)
+    identifier: Mapped[str] = mapped_column(String(17))
+    # Photometer sensor model
     sensor: Mapped[SensorType] = mapped_column(SensorType, default=Sensor.TSL237)
-    fov: Mapped[Optional[float]]  # Field of view in degrees
+    # Field of view in degrees
+    fov: Mapped[Optional[float]]
+    # Photometer Zero Point (TAS only)
     zero_point: Mapped[Optional[float]]  # Zero Point id known (i.e. TAS)
-    comment: Mapped[Optional[str]] = mapped_column(String(255))  # Photometer level comment
+    # Photometer level comment
+    comment: Mapped[Optional[str]] = mapped_column(String(255))
 
     __table_args__ = (
         UniqueConstraint(name, identifier),
@@ -230,17 +266,24 @@ class ObservationSet(Model):
     __tablename__ = "observation_set_t"
 
     obs_set_id: Mapped[int] = mapped_column(primary_key=True)
-    digest: Mapped[str] = mapped_column(String(64), unique=True)  # MD5 hash
-    temperature_1: Mapped[Optional[float]]  # See flags for details
+    # MD5 File digest to avoid duplicates
+    digest: Mapped[str] = mapped_column(String(64), unique=True)
+    # Temperature in Celsius, see flags for meaning
+    temperature_1: Mapped[Optional[float]]
+    # Temperature in Celsius, see flags for meaning
     temperature_2: Mapped[Optional[float]]
-    humidity_1: Mapped[Optional[float]]  # See flags for details
+    # Humidity, see flags for meaning
+    humidity_1: Mapped[Optional[float]]
+    # Humidity, see flags for meaning
     humidity_2: Mapped[Optional[float]]
-    weather_conditions: Mapped[Optional[str]] = mapped_column(
-        String(255)
-    )  # weather conditions In free text form
-    comment: Mapped[Optional[str]] = mapped_column(String(255))  # Observation set comment
-    other_observers: Mapped[Optional[str]] = mapped_column(String(255))  # other observers list
-    image_url: Mapped[Optional[str]] = mapped_column(String(255))  # site image as an URL
+    # Weather conditions in free text form
+    weather_conditions: Mapped[Optional[str]] = mapped_column(String(255))
+    # Additional comments
+    comment: Mapped[Optional[str]] = mapped_column(String(255))
+    # Other observers list (comma separated)
+    other_observers: Mapped[Optional[str]] = mapped_column(String(255))
+    # Site image URL
+    image_url: Mapped[Optional[str]] = mapped_column(String(255))
 
 
 class Observation(Model):
@@ -254,10 +297,17 @@ class Observation(Model):
     phot_id: Mapped[int] = mapped_column(ForeignKey("photometer_t.phot_id"))
     obs_set_id: Mapped[int] = mapped_column(ForeignKey("observation_set_t.obs_set_id"))
     flags_id: Mapped[int] = mapped_column(ForeignKey("flags_t.flags_id"))
-
-    azimuth: Mapped[float]  # In decimal degrees
-    altitude: Mapped[float]  # In decimal degrees
-    zenital: Mapped[float]  # 90 - altitude
+    # Azimuth in decimal degrees
+    azimuth: Mapped[float]
+    # Altitude in decimal degrees
+    altitude: Mapped[float]
+    # Zenital distance in decimal degrees
+    zenital: Mapped[float]
+    # Magnitudein mag/arcsec^2
     magnitude: Mapped[float]
-    frequency: Mapped[Optional[float]]  # Only for TAS
-    sky_temp: Mapped[Optional[float]]  # Only for TAS
+    # Photometer frequeincy in Hz, TAS only
+    frequency: Mapped[Optional[float]]
+    # Infrarred temnperature in Celsious, TAS only
+    sky_temp: Mapped[Optional[float]]
+    # Battery voltage, TAS only
+    vbat: Mapped[Optional[float]]
