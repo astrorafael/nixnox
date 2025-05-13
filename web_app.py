3af76c79ac1d19bbd4232ef60fa24715d5e7cecf
Rafael Gonzalez <astrorafael@yahoo.es>
2025-05-13 13:01:06 +0200

    added items to web_app

--- web_app.py
+++ web_app.py
@@ -25,33 +25,34 @@ def database_models():
         Measurement,
         Date,
         Time,
-      
     )
-    return Photometer, Observer, Observation, Location,  Date, Time, Measurement
 
-def sqa_logging(verbose: bool) -> None:
-    if verbose:
-        logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
-        # This is needed, otherwise aiosqlite msgs will jump in
-        logging.getLogger("aiosqlite").setLevel(logging.INFO)
-    else:
-        logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+    return Photometer, Observer, Observation, Location, Date, Time, Measurement
 
 
-conn = st.connection('nixnox_db', type='sql')
-
+conn = st.connection("nixnox_db", type="sql")
 Photometer, Observer, Observation, Location, Date, Time, Measurement = database_models()
 
-def available_observations(conn):
-    with conn.session as session:
-    	q = (select(Observation.identifier, Date.sql_date,  Observer.name, Location.place).distinct()
-        .select_from(Measurement)
-        .join(Date,  Measurement.date_id == Date.date_id)
-        .join(Observation, Measurement.obs_id == Observation.obs_id)
-        .join(Location, Measurement.observer_id == Location.location_id)
-        .join(Observer, Measurement.observer_id == Observer.observer_id)
+
+@st.cache_data(ttl=3600)
+def available_observations(_conn):
+    with _conn.session as session:
+        q = (
+            select(
+                Observation.identifier.label("tag"),
+                Observation.timestamp_1.label("date"),
+                Observer.name,
+                Location.place,
+            )
+            .distinct()
+            .select_from(Measurement)
+            .join(Date, Measurement.date_id == Date.date_id)
+            .join(Observation, Measurement.obs_id == Observation.obs_id)
+            .join(Location, Measurement.observer_id == Location.location_id)
+            .join(Observer, Measurement.observer_id == Observer.observer_id)
         )
-    	return session.execute(q).all()
+        return session.execute(q).all()
+
 
 st.write("**Available observations**")
 st.dataframe(available_observations(conn))
