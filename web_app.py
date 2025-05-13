7e11943c6436d7d9922a4e0e41d52d1360d14330
Rafael Gonzalez <astrorafael@yahoo.es>
2025-05-13 13:27:15 +0200

    simplidied web_app

--- web_app.py
+++ web_app.py
@@ -1,5 +1,3 @@
-import logging
-
 import streamlit as st
 from sqlalchemy import select
 
@@ -14,26 +12,22 @@ from nixnox.lib import (
     ValidState,
 )
 
+from nixnox.lib.dbase.model import (
+    Photometer,
+    Observer,
+    Observation,
+    Location,
+    Measurement,
+    Date,
+    Time,
+)
+
 # ---------------------
 # Convenience functions
 # ---------------------
 
-@st.cache_resource
-def database_models():
-    from nixnox.lib.dbase.model import (
-        Photometer,
-        Observer,
-        Observation,
-        Location,
-        Measurement,
-        Date,
-        Time,
-    )
 
-    return Photometer, Observer, Observation, Location, Date, Time, Measurement
-
-
-@st.cache_data(ttl=3600)
+@st.cache_data(ttl=60)
 def available_observations(_conn):
     with _conn.session as session:
         q = (
@@ -42,6 +36,7 @@ def available_observations(_conn):
                 Observation.timestamp_1.label("date"),
                 Observer.name,
                 Location.place,
+                Photometer.name.label("photometer"),
             )
             .distinct()
             .select_from(Measurement)
@@ -49,15 +44,17 @@ def available_observations(_conn):
             .join(Observation, Measurement.obs_id == Observation.obs_id)
             .join(Location, Measurement.observer_id == Location.location_id)
             .join(Observer, Measurement.observer_id == Observer.observer_id)
+            .join(Photometer, Measurement.phot_id == Photometer.phot_id)
         )
         return session.execute(q).all()
+
 
 # ----------------------
 # Start the ball rolling
 # ----------------------
 
 conn = st.connection("nixnox_db", type="sql")
-Photometer, Observer, Observation, Location, Date, Time, Measurement = database_models()
+# Photometer, Observer, Observation, Location, Date, Time, Measurement = database_models()
 
 st.write("**Available observations**")
 st.dataframe(available_observations(conn))
