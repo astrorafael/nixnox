f0510d786c36c4f608301b2b99f0de4f038b67cd
astrorafael <astrorafael@gmail.com>
2025-05-16 13:02:34 +0200

    new observation page

--- web_app.py
+++ web_app.py
@@ -4,7 +4,12 @@ import streamlit as st
 pg = st.navigation(
     [
         st.Page(os.path.join("pages", "home.py"), title="Listing"),
-        st.Page(os.path.join("pages", "data_entry.py"), title="New observation", icon=":material/add_circle:"),
+        st.Page(os.path.join("pages", "observation.py"), title="Observation details"),
+        st.Page(
+            os.path.join("pages", "data_entry.py"),
+            title="Upload observation file",
+            icon=":material/add_circle:",
+        ),
     ]
 )
 st.set_page_config(page_title="NIXNOX")
