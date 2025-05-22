d3eb0311b62aa807ef07d4b2fd9edbd69062a9fe
astrorafael <astrorafael@gmail.com>
2025-05-22 15:59:31 +0200

    updated home page

--- pages/home.py
+++ pages/home.py
@@ -33,6 +33,7 @@ OBSERVATION_LIMIT = 10
 # Convenience functions
 # ---------------------
 
+
 @st.cache_data(ttl=ttl())
 def obs_summary(_conn, limit):
     with _conn.session as session:
@@ -64,63 +65,87 @@ def selected_obs() -> None:
 
 
 def procesa() -> None:
-    search_results = {
+    search_conditions = {
         k: st.session_state[k] for k in st.session_state.keys() if k.startswith("search_")
     }
-    st.write(search_results)
+    search_conditions["search_by_phot_model"] = PhotometerModel(
+        search_conditions["search_by_phot_model"]
+    )
+    search_conditions["search_by_observer_type"] = ObserverType(
+        search_conditions["search_by_observer_type"]
+    )
+    with conn.session as session:
+        resultado = db.obs_summary_search(session, search_conditions)
+        st.write(resultado)
+        st.session_state.obs_list = resultado
 
 
 def form(on_submit: Callable) -> None:
     with st.form("Search", clear_on_submit=True):
         st.write("## Observations finder")
         with st.expander("Filter by date range"):
-            ancient = date(2000, 1, 1,)
+            ancient = date(
+                2000,
+                1,
+                1,
+            )
             today = date.today()
-            month_ago = today - relativedelta(months=1)
+            #month_ago = today - relativedelta(months=1)
             st.date_input(
                 "Range",
-                value=(month_ago, today),
+                value=(ancient, today),
                 min_value=ancient,
                 max_value=today,
                 key="search_date_range",
             )
         with st.expander("Filter by location range"):
-            st.markdown("**Enter coordinates OR location below**")
+            st.markdown("**Enter coordinates range OR location below**")
             coords_c, text_c = st.columns(2)
             with coords_c:
                 with st.container(border=True):
                     c1, c2 = st.columns(2)
                     with c1:
                         st.number_input(
-                            "From Longitude",
+                            "From Longitude (deg.)",
                             value=None,
                             min_value=-180,
                             max_value=180,
                             key="search_from_longitude",
                         )
                         st.number_input(
-                            "To Longitude",
-                            value=None,
-                            min_value=-180,
-                            max_value=180,
-                            key="search_to_longitude",
-                        )
-                    with c2:
-                        st.number_input(
-                            "From Latitude",
+                            "From Latitude (deg.)",
                             value=None,
                             min_value=-90,
                             max_value=90,
                             key="search_from_latitude",
                         )
+
+                    with c2:
                         st.number_input(
-                            "To Latitude", value=None, min_value=-90, max_value=90, key="search_to_latitude"
+                            "To Longitude (deg.)",
+                            value=None,
+                            min_value=-180,
+                            max_value=180,
+                            key="search_to_longitude",
+                        )
+
+                        st.number_input(
+                            "To Latitude (deg.)",
+                            value=None,
+                            min_value=-90,
+                            max_value=90,
+                            key="search_to_latitude",
                         )
             with text_c:
                 with st.container(border=True):
-                    st.selectbox("Scope", ["Population Centre", "Country"], key="search_by_location_scope")
+                    st.selectbox(
+                        "Scope", ["Population Centre", "Country"], key="search_by_location_scope"
+                    )
                     st.text_input(
-                        "Population centre or Country", key="search_by_location_value", value=None, max_chars=80
+                        "Population centre or Country",
+                        key="search_by_location_name",
+                        value=None,
+                        max_chars=80,
                     )
         with st.expander("Filter by observer"):
             st.selectbox(
@@ -140,15 +165,16 @@ def form(on_submit: Callable) -> None:
         )
 
 
-def results(conn, limit):
+def header(conn) -> None:
+    st.title("**Available observations**")
     if "obs_list" not in st.session_state:
         st.session_state.obs_list = obs_summary(conn, OBSERVATION_LIMIT)
     N = obs_nsummaries(conn)
-    st.write(f"""
-    There are {obs_nsummaries(conn)} observations available.
+    st.write(f"There are {obs_nsummaries(conn)} observations available.")
 
-    The most recent {min(N, limit)} observations are displayed:
-    """)
+
+def results(resultset, limit):
+    st.write(f"Up to {limit} observations are displayed:")
     st.dataframe(
         st.session_state.obs_list,
         key="ObservationDF",
@@ -167,12 +193,13 @@ def results(conn, limit):
             icon=":material/download:",
         )
 
+
 # ----------------------
 # Start the ball rolling
 # ----------------------
 
 conn = st.connection("env:NX_ENV", type="sql")
-st.title("**Available observations**")
-results(conn, limit=OBSERVATION_LIMIT)
+header(conn)
+results(resultset=st.session_state.obs_list, limit=OBSERVATION_LIMIT)
 st.divider()
 form(on_submit=procesa)
