d9e9b7b8a86569f5fe158e650aedad6d4d31abfe
Rafael Gonzalez <astrorafael@gmail.com>
2025-06-02 12:08:44 +0200

    fixed missing page changes

--- pages/observer.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-from nixnox.web.page.observer import org_init, person_init, view_all
-
-conn = st.connection("env:NX_ENV", type="sql")
-
-org_init(conn)
-person_init(conn)
-view_all(conn)
