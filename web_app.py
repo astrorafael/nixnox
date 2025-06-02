8c74a430f56bbeee31a126ea0d082195643ee959
Rafael Gonzalez <astrorafael@gmail.com>
2025-06-02 09:42:56 +0200

    sidebar with sections

--- web_app.py
+++ web_app.py
@@ -15,32 +15,36 @@ import streamlit as st
 # ==========================
 st.set_page_config(page_title="NIXNOX", layout="wide", page_icon=":material/moon_stars:")
 pg = st.navigation(
-    [
-        st.Page(
-            os.path.join("pages", "home.py"),
-            title="Observations List",
-            icon=":material/list:",
-        ),
-        st.Page(
-            os.path.join("pages", "observation.py"),
-            title="Observation details",
-            icon=":material/zoom_in:",
-        ),
-        st.Page(
-            os.path.join("pages", "plot.py"),
-            title="Observation plot",
-            icon=":material/graph_7:",
-        ),
-        st.Page(
-            os.path.join("pages", "upload.py"),
-            title="Upload observation file",
-            icon=":material/upload_file:",
-        ),
-        st.Page(
-            os.path.join("pages", "observer.py"),
-            title="Edit observer",
-            icon=":material/edit:",
-        ),
-    ]
+    {
+        "General": [
+            st.Page(
+                os.path.join("pages", "home.py"),
+                title="Observations List",
+                icon=":material/list:",
+            ),
+            st.Page(
+                os.path.join("pages", "observation.py"),
+                title="Observation details",
+                icon=":material/zoom_in:",
+            ),
+            st.Page(
+                os.path.join("pages", "plot.py"),
+                title="Observation plot",
+                icon=":material/graph_7:",
+            ),
+            st.Page(
+                os.path.join("pages", "upload.py"),
+                title="Upload observation file",
+                icon=":material/upload_file:",
+            ),
+        ],
+        "Administration": [
+            st.Page(
+                os.path.join("pages", "observer.py"),
+                title="Edit observer",
+                icon=":material/edit:",
+            ),
+        ],
+    }
 )
 pg.run()
